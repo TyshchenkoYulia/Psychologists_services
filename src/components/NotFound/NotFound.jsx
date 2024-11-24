80fe0540708c9053ff2f3c4a976025f0a817ee94
TyshchenkoYulia <145122847+TyshchenkoYulia@users.noreply.github.com>
2024-11-24 23:33:32 +0200

    added loader and notFound page

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,7 +1,18 @@
+import css from "./NotFound.module.css";
+import { FaPeoplePulling } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+
 export default function NotFound() {
   return (
-    <div>
-      <p>Not Found !!!</p>
+    <div className={css.container}>
+      <p className={css.text}>Opps! Page not found! Sorry!</p>
+      <p className={css.message}>
+        Please, visit
+        <Link className={css.link} to="/">
+          home page
+        </Link>
+      </p>
+      <FaPeoplePulling className={css.icon} size={80} />
     </div>
   );
 }
